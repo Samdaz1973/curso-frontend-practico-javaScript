08dce6624da8883fc3e535699ffdf9af8feb6e08
Samuel Daza <samdaza73@gmail.com>
2022-09-07 20:33:28 -0500

    Fusión orden de compra del carrito con las otras ventanas flotantes, menú lateral mobile y cuenta de usuario

--- main.js
+++ main.js
@@ -1,30 +1,48 @@
-// !para activar el menú flotante, primero seleccionamos el elemento li de donde se despliega el menú flotante y también la clase del descktop-menu
+// para activar y desactivar las ventanas flotantes, ponemos en el css la clase inactive con display none; y luego la asignamos a las ventanas que querramos desactivar.
 
+// *DESPLEGABLE MENÚ EMAIL ORDEN Y CUENTA DE USUARIO
+//  1. seleccionamos la clase del li del menú de donde se hace click para que se despliegue la ventana flotante y la guardamos en una variable
+//  También seleccionamos la clase del contenedor de la ventana que se va a desplegar y la guardamos en otra variable
 const menuEmail = document.querySelector('.navbar-email');
 const descktopMenu = document.querySelector('.desktop-menu');
 
-// *selector del ícono de menú de mobile
-const menuMobiHambur = document.querySelector('.menu')
-
-// *selector de la barra vertical de menú de mobile
-const mobileMenu = document.querySelector('.mobile-menu')
-
-// utilizamos el método Listener para provocar una acción ver u ocultar
+// 2. utilizamos el método Listener para provocar una acción ver u ocultar usando la variable const defina anteriormente
 menuEmail.addEventListener('click', toggleDesktopMenu);
-menuMobiHambur.addEventListener('click', toggleMobileMenu);
 
-// creamos la función para ejecutar el método anterior
+//  3. creamos la función del toggle para ejecutar dentro, el código que hace aparecer o desaparecer la ventana flotante; el nombre de la función tiene que coincidir con el argumento del Listener (toggleDesktopMenu); hay que ponerle en el html, al contenedor de la ventana desplegable, la clase inactive
 function toggleDesktopMenu() {
-    //*el código falló; una manera de probarlo es haciendo un console.log para ver si está escuchando el click
-    //  console.log('click');
-    //la clase inactive está en el css y es lo que permite ocultar el descktop-menu; con esta función la activamos o desactivamos
+    aside.classList.add('inactive');//esta linea se incluyó para que no se superponga la ventana de la cuenta de usuario; si se abre el aside, se le agrega la clase inactive a este y se cierra
     descktopMenu.classList.toggle('inactive');
 }
 
-// este otro console es para ver si el archivo está conectado y funcionando
-//  console.log('JS funcionando'); 
-//  el archivo no estataba conectado en la etiqueta script en el html
+// *SELECTOR DEL ICONO DE MENÚ LATERAL MOBILE
+// 1.
+const menuMobiHambur = document.querySelector('.menu');
+const mobileMenu = document.querySelector('.mobile-menu')
 
+// 2.
+menuMobiHambur.addEventListener('click', toggleMobileMenu);
+
+// 3.
 function toggleMobileMenu() {
+    aside.classList.add('inactive');//esta linea se incluyó para que no se superponga con la orden de compra del carrito; si se abre el aside, se le agrega la clase inactive a este y se cierra
     mobileMenu.classList.toggle('inactive');
 }
+
+// *SELECTOR CARRITO Y VENTANA ORDEN DE COMPRA
+// 1.
+const menuCarritoIcon = document.querySelector('.navbar-shopping-cart');
+const aside = document.querySelector('.product-detail');
+
+// 2.
+menuCarritoIcon.addEventListener('click', toggleCarritoAside);
+
+// 3.
+function toggleCarritoAside() {
+    descktopMenu.classList.add('inactive'); //si se abre la ventana de cuenta de usuario la orden de compra se cierra 
+    mobileMenu.classList.add('inactive');//esta linea se incluyó para que no se superponga con la barra del menú lateral; si se abre el mobilMenu, se le agrega la clase inactive a este y se cierra.
+    aside.classList.toggle('inactive');
+}
+
+//*si el código falla; una manera de probarlo es haciendo un console.log para ver si está escuchando el click; hacemos un  console.log('click');
+    
