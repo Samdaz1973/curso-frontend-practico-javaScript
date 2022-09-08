5d58b1b2827006c6e7f2fa233d439519a7ee1c28
Samuel Daza <samdaza73@gmail.com>
2022-09-08 17:58:46 -0500

    Agregué ventana de detalle de productos y la intereacción entre todos los componentes

--- main.js
+++ main.js
@@ -11,7 +11,9 @@ menuEmail.addEventListener('click', toggleDesktopMenu);
 
 //  3. creamos la función del toggle para ejecutar dentro, el código que hace aparecer o desaparecer la ventana flotante; el nombre de la función tiene que coincidir con el argumento del Listener (toggleDesktopMenu); hay que ponerle en el html, al contenedor de la ventana desplegable, la clase inactive
 function toggleDesktopMenu() {
-    aside.classList.add('inactive');//esta linea se incluyó para que no se superponga la ventana de la cuenta de usuario; si se abre el aside, se le agrega la clase inactive a este y se cierra
+    shoppingCartContainer.classList.add('inactive');//esta linea se incluyó para que no se superponga la ventana de la cuenta de usuario; si se abre el aside, se le agrega la clase inactive a este y se cierra
+    productDetailContainer.classList.add('inactive');
+
     descktopMenu.classList.toggle('inactive');
 }
 
@@ -25,26 +27,49 @@ menuMobiHambur.addEventListener('click', toggleMobileMenu);
 
 // 3.
 function toggleMobileMenu() {
-    aside.classList.add('inactive');//esta linea se incluyó para que no se superponga con la orden de compra del carrito; si se abre el aside, se le agrega la clase inactive a este y se cierra
+    shoppingCartContainer.classList.add('inactive');//esta linea se incluyó para que no se superponga con la orden de compra del carrito; si se abre el aside, se le agrega la clase inactive a este y se cierra
+    productDetailContainer.classList.add('inactive');
     mobileMenu.classList.toggle('inactive');
 }
 
 // *SELECTOR CARRITO Y VENTANA ORDEN DE COMPRA
 // 1.
 const menuCarritoIcon = document.querySelector('.navbar-shopping-cart');
-const aside = document.querySelector('.product-detail');
+const shoppingCartContainer = document.querySelector('#shoppingCartContainer');
 
 // 2.
 menuCarritoIcon.addEventListener('click', toggleCarritoAside);
 
 // 3.
 function toggleCarritoAside() {
+    productDetailContainer.classList.add('inactive');
     descktopMenu.classList.add('inactive'); //si se abre la ventana de cuenta de usuario la orden de compra se cierra 
     mobileMenu.classList.add('inactive');//esta linea se incluyó para que no se superponga con la barra del menú lateral; si se abre el mobilMenu, se le agrega la clase inactive a este y se cierra.
-    aside.classList.toggle('inactive');
+    shoppingCartContainer.classList.toggle('inactive');
+    
+}
+//  *selector y función para abrir ventana de producto al hacer click en las fotos
+const productDetailContainer = document.querySelector('#productDetail');
+function openProductDetailAside() {
+    shoppingCartContainer.classList.add('inactive')
+    descktopMenu.classList.add('inactive');
+    mobileMenu.classList.add('inactive');
+    productDetailContainer.classList.remove('inactive');
+}
+//  *selector y función para cerrar ventana de producto al hacer click en las fotos
+const productDetailCloseIcon = document.querySelector('.product-detail-close')
+
+productDetailCloseIcon.addEventListener('click', closeProductDetailAside)
+
+function closeProductDetailAside() {
+    productDetailContainer.classList.add('inactive');
 }
 
 //*si el código falla; una manera de probarlo es haciendo un console.log para ver si está escuchando el click; hacemos un  console.log('click');
+
+
+// * CÓDIGO PARA ACOPLAR LA VENTANA DE PRODUCTOS:
+
 // creamos un array con varios objetos
 const productList = [];
 productList.push({
@@ -63,9 +88,8 @@ productList.push({
     image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
 });
 
-
+//* este código de html lo maquetamos en JS dentro del for
 /*
-este código de html lo maquetamos en JS dentro del for
     <div class="product-card">
         <img src="https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" alt="">
         <div class="product-info">
@@ -81,15 +105,15 @@ este código de html lo maquetamos en JS dentro del for
 */
 // createElement nos permite crear cada elemento desde JS; adentro creamos el contenedor html
 
-const cardsContainer = document.querySelector('.cards-container')
-
-// para insertarlo en el html, tenemos que recorrer el array; podemos hacer un ciclo for; hay una sintaxis que simplifica el for; otra forma es con in en vez del of, funciona imprimiendo el índice no las propiedades
+// para insertarlo en el html, tenemos que recorrer el array; podemos hacer un ciclo for; hay una sintaxis que simplifica el for; otra opción dentro del for es con in en vez del of, funciona imprimiendo el índice no las propiedades
 /*
  for (product in productList) {
      
  }
  */
-
+// este es el contenedor más general; la función append mete un contenedor en otro; se hace desde el más interior hasta el más general o exterior
+const cardsContainer = document.querySelector('.cards-container');
+/*
 for (product of productList) {
     const productCard = document.createElement('div');
     productCard.classList.add('product-card');
@@ -121,9 +145,9 @@ for (product of productList) {
 
     cardsContainer.appendChild(productCard);
 }
+*/
 
-/*
-otra manera de trabajar el for dentro de una función
+// otra manera de trabajar el for dentro de una función
 function renderProducts(arr) {
     for (product of arr) {
         const productCard = document.createElement('div');
@@ -131,6 +155,7 @@ function renderProducts(arr) {
     
         const productImg = document.createElement('img');
         productImg.setAttribute('src', product.image);
+        productImg.addEventListener('click', openProductDetailAside);
     
         const productInfo = document.createElement('div');
         productInfo.classList.add('product-info');
@@ -157,6 +182,6 @@ function renderProducts(arr) {
         cardsContainer.appendChild(productCard);
     }
 }
-Luego llamamos esa función; incluso la podríamos accionar desde un click en la página
+// Luego llamamos esa función; incluso la podríamos accionar desde un click en la página
 renderProducts(productList);
-*/
+
