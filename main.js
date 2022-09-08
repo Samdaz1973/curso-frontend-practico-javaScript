13edb3eba7d25d419cc882062696d53d8769ff9e
Samuel Daza <samdaza73@gmail.com>
2022-09-07 23:34:26 -0500

    fusionar la lista de productos, maquetar desde JS, desde un array

--- main.js
+++ main.js
@@ -45,4 +45,118 @@ function toggleCarritoAside() {
 }
 
 //*si el código falla; una manera de probarlo es haciendo un console.log para ver si está escuchando el click; hacemos un  console.log('click');
+// creamos un array con varios objetos
+const productList = [];
+productList.push({
+    name: 'Bike',
+    price: 120,
+    image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+});
+productList.push({
+    name: 'Monitor',
+    price: 320,
+    image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+});
+productList.push({
+    name: 'Compu',
+    price: 620,
+    image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+});
+
+
+/*
+este código de html lo maquetamos en JS dentro del for
+    <div class="product-card">
+        <img src="https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" alt="">
+        <div class="product-info">
+          <div>
+            <p>$120,00</p>
+            <p>Bike</p>
+          </div>
+          <figure>
+            <img src="./icons/bt_add_to_cart.svg" alt="">
+          </figure>
+        </div>
+      </div>
+*/
+// createElement nos permite crear cada elemento desde JS; adentro creamos el contenedor html
+
+const cardsContainer = document.querySelector('.cards-container')
+
+// para insertarlo en el html, tenemos que recorrer el array; podemos hacer un ciclo for; hay una sintaxis que simplifica el for; otra forma es con in en vez del of, funciona imprimiendo el índice no las propiedades
+/*
+ for (product in productList) {
+     
+ }
+ */
+
+for (product of productList) {
+    const productCard = document.createElement('div');
+    productCard.classList.add('product-card');
+
+    const productImg = document.createElement('img');
+    productImg.setAttribute('src', product.image);
+
+    const productInfo = document.createElement('div');
+    productInfo.classList.add('product-info');
     
+    const productInfoDiv = document.createElement('div');
+
+    const productPrice = document.createElement('p');
+    productPrice.innerText = '$' + product.price;
+    const productName = document.createElement('p');
+    productName.innerText = product.name;
+
+    productInfoDiv.append(productPrice, productName);
+
+    const productInfoFigure = document.createElement('figure');
+    const productImgCart = document.createElement('img');
+    productImgCart.setAttribute('src', './icons/bt_add_to_cart.svg');
+
+    productInfoFigure.appendChild(productImgCart);
+
+    productInfo.append(productInfoDiv, productInfoFigure);
+
+    productCard.append(productImg, productInfo);
+
+    cardsContainer.appendChild(productCard);
+}
+
+/*
+otra manera de trabajar el for dentro de una función
+function renderProducts(arr) {
+    for (product of arr) {
+        const productCard = document.createElement('div');
+        productCard.classList.add('product-card');
+    
+        const productImg = document.createElement('img');
+        productImg.setAttribute('src', product.image);
+    
+        const productInfo = document.createElement('div');
+        productInfo.classList.add('product-info');
+        
+        const productInfoDiv = document.createElement('div');
+    
+        const productPrice = document.createElement('p');
+        productPrice.innerText = '$' + product.price;
+        const productName = document.createElement('p');
+        productName.innerText = product.name;
+    
+        productInfoDiv.append(productPrice, productName);
+    
+        const productInfoFigure = document.createElement('figure');
+        const productImgCart = document.createElement('img');
+        productImgCart.setAttribute('src', './icons/bt_add_to_cart.svg');
+    
+        productInfoFigure.appendChild(productImgCart);
+    
+        productInfo.append(productInfoDiv, productInfoFigure);
+    
+        productCard.append(productImg, productInfo);
+    
+        cardsContainer.appendChild(productCard);
+    }
+}
+Luego llamamos esa función; incluso la podríamos accionar desde un click en la página
+renderProducts(productList);
+*/
